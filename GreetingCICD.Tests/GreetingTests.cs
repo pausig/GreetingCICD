65ac725b909d21c086b375496f2cad1a46cb08f4
Riccardo <riccardo.pausig@gmail.com>
2025-01-15 09:07:14 +0100

    Add project files.

---
+++ GreetingCICD.Tests/GreetingTests.cs
@@ -0,0 +1,11 @@
+namespace GreetingCICD.Tests
+{
+    public class GreetingTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ GreetingCICD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GreetingCICD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
